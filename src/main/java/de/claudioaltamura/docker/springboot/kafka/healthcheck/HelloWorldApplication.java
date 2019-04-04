0d4d927af2c34a5d09eb51989c706d311edaa547
Claudio Altamura <claudio@claudioaltamura.de>
2019-04-04 09:43:22 +0200

    chg: enable kafka

--- src/main/java/de/claudioaltamura/docker/springboot/kafka/healthcheck/HelloWorldApplication.java
+++ src/main/java/de/claudioaltamura/docker/springboot/kafka/healthcheck/HelloWorldApplication.java
@@ -2,11 +2,13 @@ package de.claudioaltamura.docker.springboot.kafka.healthcheck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
-public class Application {
+public class HelloWorldApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(HelloWorldApplication.class, args);
   }
 }
