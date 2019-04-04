040425e95eaf12b7f2ad42bb99e0b7e17c70baae
Claudio Altamura <claudio@claudioaltamura.de>
2019-04-04 09:43:00 +0200

    chore: cleaned class

--- src/main/java/de/claudioaltamura/docker/springboot/kafka/healthcheck/HelloWorldMessage.java
+++ src/main/java/de/claudioaltamura/docker/springboot/kafka/healthcheck/HelloWorldMessage.java
@@ -3,20 +3,21 @@ package de.claudioaltamura.docker.springboot.kafka.healthcheck;
 public class HelloWorldMessage {
 
   private long timestamp;
+
   private String message;
-  
+
   public long getTimestamp() {
     return timestamp;
   }
-  
+
   public void setTimestamp(long timestamp) {
     this.timestamp = timestamp;
   }
-  
+
   public String getMessage() {
     return message;
   }
-  
+
   public void setMessage(String message) {
     this.message = message;
   }
